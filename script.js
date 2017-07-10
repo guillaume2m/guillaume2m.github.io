8130b241d253c497c276f8c013ef0389914c0b78
guillaume2m <guillaumedemilleville@gmail.com>
2017-07-10 19:33:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-confirm("Ce site a été concu afin d'être entièrement scrollé et visioné sur ordinateur en raison de la qualité des photos. La bise, Guillaume");
+confirm("Ce site a été concu afin d'être visioné uniquement sur ordinateur afin d'optimiser la qualité des photos. La bise, Guillaume");
